453230420348566c4806bb90c5260587fa3dfddc
NikWhit <135679785+NikWhit@users.noreply.github.com>
2023-12-18 17:15:33 -0700

    overhaul on server.js, added to package.json for npm start, and began work on my routes

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+//Import file with notes router
+const notesRouter = require('./notes');
+
+//use express instance
+const app = express();
+
+//middleware to traverse the notes route
+app.use('/notes', notesRouter);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,41 +1,47 @@
 const express = require('express');
+const fs = require('fs');
 const path = require ('path');
 // Source is in 21_Modular-Routing/server.js
-const { readFromFile, readAndAppend } =require("./helpers/fsUtils");
-const uuid = require( './helpers/uuid')
-
-const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-//midware
+const PORT = process.env.PORT || 3001;
+
+//middleware for JSON Parsing
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // Static middleware pointing to the public folder//11-Express/01-Ins_Setup
 app.use(express.static('public'));
 
+//Requests that have /api to the index.js in the routes folder
+app.use('/api', api);
+
 //--22-Stu_Modular-Routing
 app.get('/api/notes', (req, res) => {
-    readFromFile("./db/db.json").then((data) => res.json(json.parse(data)));
+    const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+    res.json(notes);
+    // readFromFile("./db/db.json").then((data) => res.json(json.parse(data)));
 });
 
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    );
+
 app.post('/api/notes', (req, res) => {
-    const newNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uuid()
-    }
-    readAndAppend (newNote, "./db/db.json");
-    res.json('note made');
-});
-//GET route for notes
-app.get('/notes', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/notes.html')));
-//05-Ins_Query-Params
-//GET route for index.html
-app.get('/index', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/index.html')));
+    const newNote = req.body;
+    newNote.id = Date.now().toString();
+
+    const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+    notes.push(newNote);
+
+    fs.writeFileSync('db.json', JSON.stringify(notes));
+    res.json(newNote);
+    });
 
 app.listen(PORT, () =>
 console.log(`App listening at http://localhost:${PORT}`));
